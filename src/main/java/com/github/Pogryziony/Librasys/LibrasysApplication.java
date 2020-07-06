8e9ef7aca5e80518e711841535d1470f3590f689
Pogryziony <Pogryziony98@gmail.com>
2020-07-06 17:44:06 +0200

    Initial commit

---
+++ src/main/java/com/github/Pogryziony/Librasys/LibrasysApplication.java
@@ -0,0 +1,13 @@
+package com.github.Pogryziony.Librasys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrasysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrasysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/Pogryziony/Librasys/LibrasysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Pogryziony.Librasys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrasysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
